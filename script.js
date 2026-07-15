dce05a867a8b0328f37286e806684f5823f5656b
HarlemGuatemala <harlem3010@gmail.com>
2026-07-14 20:01:11 -0600

    Version4

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', () => {
     
     // Si el usuario bajó más de 200px, le añadimos la clase para ocultarlo.
     // Si vuelve arriba, se la quitamos para que vuelva a aparecer.
-    if (scrollActual > 200) {
+    if (scrollActual > 100) {
         header.classList.add('header-oculto');
     } else {
         header.classList.remove('header-oculto');
